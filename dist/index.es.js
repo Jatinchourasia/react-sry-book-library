3ef279f6a24b0ee79703a72639e5ccfa141664d9
jatin19 <jatin@bitcs.in>
2021-09-20 17:03:46 +0530

    removed dist and rollup config

--- dist/index.es.js
+++
@@ -1,1 +0,0 @@
-const e=()=>React.createElement(React.Fragment,null,"this is Hello Component");export{e as Requirement};
--- dist/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});exports.Requirement=()=>React.createElement(React.Fragment,null,"this is Hello Component");
